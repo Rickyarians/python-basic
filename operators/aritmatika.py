6bf8f330b40aedb15ead8c8903e12efea0bf1c34
rickyarians <rickyarians@outlook.com>
2022-12-18 01:58:18 +0700

    learning phase 4

---
+++ operators/aritmatika.py
@@ -0,0 +1,32 @@
+# tambah
+x = 5 + 6
+print(x)
+
+# substraction / kurang
+x = 10 - 4
+print(x)
+
+
+# perkalian / multiplication 
+x = 10 * 4
+print(x)
+
+#  division pembagian
+x = 10 / 3
+print(x)
+
+#  division pembagian floot
+x = 10 // 3
+print(x)
+
+# modulus
+x = 10 % 3
+print(x)
+
+# exponentiation
+x = 10 ** 2
+print(x)
+
+
+
+
---
+++ operators/assignment.py
@@ -0,0 +1,40 @@
+# = 
+x = 5
+print(x)
+
+x += 3
+print(x)
+
+x -= 3
+print(x)
+
+x *= 3
+print(x)
+
+y = 10
+y /= 3
+print(y)
+
+y = 10
+y //= 3
+print(y)
+
+y = 10
+y **= 3
+print(y)
+
+y = 10
+y %= 3
+print(y)
+
+
+c = 10
+print(2 & 2)
+c = 5 & 3
+print("&", c)
+
+a = 14
+b = 16
+  
+print(b and a) # print_stat1
+print(b | a) # print_stat2
