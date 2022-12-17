edb8d2463d1a10ec5b30eebbfebb4b80b2e0068d
rickyarians <rickyarians@outlook.com>
2022-12-17 16:55:40 +0700

    learn pthon phase 1

---
+++ hello-world.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+
+# cek version
+# python --version
+
+
+# run with python3 
---
+++ python-comment.py
@@ -0,0 +1,16 @@
+# this is comment
+if 5 > 2:
+  print("Five is greater than two!")
+
+# multiline
+# line 1
+# line 2
+# line 3
+if 5 > 2:
+  print("Five is greater than two!")
+
+"""
+ini adalah comment multiline
+anda pahaaam?
+"""
+
